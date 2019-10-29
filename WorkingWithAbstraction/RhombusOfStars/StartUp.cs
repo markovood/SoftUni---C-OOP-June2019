62cff094aeb0dd49ec04fb38a3e0f0f0189113d1
Georgi Markov <markov_ood@abv.bg>
2019-10-29 11:45:09 +0200

    Completed Rhombus task...

---
+++ WorkingWithAbstraction/RhombusOfStars/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace RhombusOfStars
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            DrawRhombus(n);
+        }
+
+        private static void DrawRhombus(int n)
+        {
+            // draw first half
+            for (int i = 1; i <= n; i++)
+            {
+                PrintLine(i, n);
+            }
+
+            // draw second half
+            for (int i = n - 1; i > 0; i--)
+            {
+                PrintLine(i, n);
+            }
+        }
+
+        private static void PrintLine(int stars, int maxSpaces)
+        {
+            string spacePart = new string(' ', maxSpaces - stars);
+            Console.Write(spacePart);
+            for (int i = 0; i < stars; i++)
+            {
+                Console.Write("* ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
