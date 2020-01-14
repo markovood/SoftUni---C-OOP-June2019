264188774031ac0e21b603fbdf07509a50011e96
Georgi Markov <markov_ood@abv.bg>
2020-01-14 16:44:47 +0200

    Re-factored Greedy times task..

---
+++ WorkingWithAbstraction - Exercise/P05_GreedyTimes/GreedyTimes.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05_GreedyTimes
+{
+
+    public class GreedyTimes
+    {
+        public static void Main()
+        {
+            long bagCapcity = long.Parse(Console.ReadLine());
+            string[] pairs = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            var bag = new Dictionary<string, Dictionary<string, long>>();
+            long gold = 0;
+            long gems = 0;
+            long cash = 0;
+
+            for (int i = 0; i < pairs.Length; i += 2)
+            {
+                string name = pairs[i];
+                long quantity = long.Parse(pairs[i + 1]);
+
+                string item = string.Empty;
+
+                if (name.Length == 3)
+                {
+                    item = "Cash";
+                }
+                else if (name.ToLower().EndsWith("gem"))
+                {
+                    item = "Gem";
+                }
+                else if (name.ToLower() == "gold")
+                {
+                    item = "Gold";
+                }
+
+                if (item == "")
+                {
+                    continue;
+                }
+                else if (bagCapcity < bag.Values.Select(x => x.Values.Sum()).Sum() + quantity)
+                {
+                    continue;
+                }
+
+                switch (item)
+                {
+                    case "Gem":
+                        if (!bag.ContainsKey(item))
+                        {
+                            if (bag.ContainsKey("Gold"))
+                            {
+                                if (quantity > bag["Gold"].Values.Sum())
+                                {
+                                    continue;
+                                }
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                        else if (bag[item].Values.Sum() + quantity > bag["Gold"].Values.Sum())
+                        {
+                            continue;
+                        }
+                        break;
+                    case "Cash":
+                        if (!bag.ContainsKey(item))
+                        {
+                            if (bag.ContainsKey("Gem"))
+                            {
+                                if (quantity > bag["Gem"].Values.Sum())
+                                {
+                                    continue;
+                                }
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                        else if (bag[item].Values.Sum() + quantity > bag["Gem"].Values.Sum())
+                        {
+                            continue;
+                        }
+                        break;
+                }
+
+                if (!bag.ContainsKey(item))
+                {
+                    bag[item] = new Dictionary<string, long>();
+                }
+
+                if (!bag[item].ContainsKey(name))
+                {
+                    bag[item][name] = 0;
+                }
+
+                bag[item][name] += quantity;
+                if (item == "Gold")
+                {
+                    gold += quantity;
+                }
+                else if (item == "Gem")
+                {
+                    gems += quantity;
+                }
+                else if (item == "Cash")
+                {
+                    cash += quantity;
+                }
+            }
+
+            foreach (var item in bag)
+            {
+                Console.WriteLine($"<{item.Key}> ${item.Value.Values.Sum()}");
+                foreach (var item2 in item.Value.OrderByDescending(y => y.Key).ThenBy(y => y.Value))
+                {
+                    Console.WriteLine($"##{item2.Key} - {item2.Value}");
+                }
+            }
+        }
+    }
+}
