8bdbe897fb402aebcdd994230239d2e8405eafb0
Georgi Markov <markov_ood@abv.bg>
2020-03-28 12:25:59 +0200

    Completed Ferrari Task...

---
+++ InterfacesAndAbstraction - Exercises/Ferrari/Ferrari.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ferrari
+{
+    public class Ferrari : ICar
+    {
+        private const string MODEL = "488-Spider";
+
+        public Ferrari(string driver)
+        {
+            this.Driver = driver;
+        }
+
+        public string Driver { get; private set; }
+
+        public string Model => MODEL;
+
+        public string PushBreaks()
+        {
+            return "Brakes!";
+        }
+
+        public string PushGas()
+        {
+            return "Gas!";
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Model}/{PushBreaks()}/{PushGas()}/{this.Driver}";
+        }
+    }
+}
---
+++ InterfacesAndAbstraction - Exercises/Ferrari/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ferrari
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string driver = Console.ReadLine();
+            var car = new Ferrari(driver);
+            Console.WriteLine(car);
+        }
+    }
+}
