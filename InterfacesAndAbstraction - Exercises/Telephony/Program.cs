a69ac332d07331c80b83569e2ebd0770d259e69d
Georgi Markov <markov_ood@abv.bg>
2020-03-28 13:37:22 +0200

    Completed Telephony task...

---
+++ InterfacesAndAbstraction - Exercises/Telephony/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Telephony
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var phone = new Smartphone();
+            Console.ReadLine().
+                Split(' ', StringSplitOptions.RemoveEmptyEntries).
+                ToList().
+                ForEach(num => phone.Call(num));
+
+            Console.ReadLine().
+                Split(' ', StringSplitOptions.RemoveEmptyEntries).
+                ToList().
+                ForEach(site => phone.Browse(site));
+        }
+    }
+}
---
+++ InterfacesAndAbstraction - Exercises/Telephony/Smartphone.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Telephony
+{
+    public class Smartphone : ITelephone, IBrowser
+    {
+        public void Call(string number)
+        {
+            if (IsValidNumber(number))
+            {
+                Console.WriteLine($"Calling... {number}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid number!");
+            }
+        }
+
+        public void Browse(string site)
+        {
+            if (IsValidURL(site))
+            {
+                Console.WriteLine($"Browsing: {site}!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid URL!");
+            }
+        }
+
+        private bool IsValidNumber(string number)
+        {
+            foreach (var symbol in number)
+            {
+                if (!char.IsDigit(symbol))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool IsValidURL(string site)
+        {
+            foreach (var symbol in site)
+            {
+                if (char.IsDigit(symbol))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
