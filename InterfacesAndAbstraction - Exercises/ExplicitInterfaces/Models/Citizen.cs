23e5f7e788c770f301d35b1441831bf4e07195c1
Georgi Markov <markov_ood@abv.bg>
2020-04-03 14:39:17 +0300

    Completed Explicit interfaces task...

---
+++ InterfacesAndAbstraction - Exercises/ExplicitInterfaces/Models/Citizen.cs
@@ -0,0 +1,30 @@
+﻿using ExplicitInterfaces.Contracts;
+
+namespace ExplicitInterfaces.Models
+{
+    public class Citizen : IPerson, IResident
+    {
+        public Citizen(string name, string country, int age)
+        {
+            this.Name = name;
+            this.Country = country;
+            this.Age = age;
+        }
+
+        string IResident.GetName()
+        {
+            return "Mr/Ms/Mrs " + this.Name;
+        }
+
+        public string GetName()
+        {
+            return this.Name;
+        }
+
+        public string Name { get; private set; }
+
+        public string Country { get; private set; }
+
+        public int Age { get; private set; }
+    }
+}
---
+++ InterfacesAndAbstraction - Exercises/ExplicitInterfaces/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ExplicitInterfaces.Contracts;
+using ExplicitInterfaces.Models;
+
+namespace ExplicitInterfaces
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line == "End")
+                {
+                    break;
+                }
+
+                string[] lineArgs = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                Citizen citizen = new Citizen(lineArgs[0], lineArgs[1], int.Parse(lineArgs[2]));
+
+                IResident resident = citizen;
+                IPerson person = citizen;
+
+                Console.WriteLine(citizen.GetName());
+                Console.WriteLine(resident.GetName());
+            }
+        }
+    }
+}
