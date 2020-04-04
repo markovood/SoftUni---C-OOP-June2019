ed315197f8f2c33803a9724060ba60562b054e96
Georgi Markov <markov_ood@abv.bg>
2020-04-04 17:15:14 +0300

    Completed Polymorphism Lab...

---
+++ Polymorphism - Lab/Shapes/Rectangle.cs
@@ -0,0 +1,30 @@
+﻿namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        public Rectangle(double height, double width)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public double Height { get; private set; }
+
+        public double Width { get; private set; }
+
+        public override double CalculateArea()
+        {
+            return this.Width * this.Height;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * this.Width + 2 * this.Height;
+        }
+
+        public override string Draw()
+        {
+            return "I am a rectangle.";
+        }
+    }
+}
