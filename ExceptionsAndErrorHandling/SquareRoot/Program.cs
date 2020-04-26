2e45e616f7752bf3bbd33a53d09273292df7c05e
Georgi Markov <markov_ood@abv.bg>
2020-04-26 14:20:26 +0300

    Completed Square root task...

--- ExceptionsAndErrorHandling/SquareRoot/Program.cs
+++ ExceptionsAndErrorHandling/SquareRoot/Program.cs
@@ -6,6 +6,30 @@ namespace SquareRoot
     {
         public static void Main()
         {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                double numberSqrt = Math.Sqrt(number);
+                if (double.IsNaN(numberSqrt))
+                {
+                    throw new ArgumentException();
+                }
+
+                Console.WriteLine(numberSqrt);
+            }
+            catch (ArithmeticException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
 
         }
     }
