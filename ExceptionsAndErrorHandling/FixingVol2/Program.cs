643310f6f45fbe36d0efdb345c65c6d7ee774734
Georgi Markov <markov_ood@abv.bg>
2020-04-26 15:09:16 +0300

    Completed Fixing Vol 2 task...

---
+++ ExceptionsAndErrorHandling/FixingVol2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FixingVol2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int num1, num2;
+            byte result;
+
+            num1 = 30;
+            num2 = 60;
+            try
+            {
+                result = Convert.ToByte(num1 * num2);
+                Console.WriteLine("{0} x {1} = {2}", num1, num2, result);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Cannot write a value bigger than unsigned Int8 ");
+            }
+        }
+    }
+}
