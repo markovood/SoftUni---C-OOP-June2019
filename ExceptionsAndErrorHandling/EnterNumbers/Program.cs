10493e49005dada335fc5d02dbf7aa717c74edb4
Georgi Markov <markov_ood@abv.bg>
2020-04-26 14:49:15 +0300

    Completed Enter numbers task...

---
+++ ExceptionsAndErrorHandling/EnterNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EnterNumbers
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            List<int> numbers = new List<int>();
+            Console.WriteLine("Enter 10 integers so that 1 < x < 100");
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    numbers.Add(ReadNumber(1, 100));
+                }
+                catch (InvalidDataException)
+                {
+                    numbers.Clear();
+                    i = 0;
+                    Console.WriteLine("Invalid number entered, please start again");
+                }
+            }
+
+            Console.WriteLine($"You entered: {string.Join(' ', numbers)}");
+        }
+
+        private static int ReadNumber(int start, int end)
+        {
+            int number = int.Parse(Console.ReadLine());
+            if (number <= start || number >= end)
+            {
+                throw new InvalidDataException("number must be 1 < x < 100");
+            }
+
+            return number;
+        }
+    }
+}
