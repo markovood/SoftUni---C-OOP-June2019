05eac28c98c22bc3571fc1bc057ebfb84f2cd10c
Georgi Markov <markov_ood@abv.bg>
2020-05-10 15:34:33 +0300

    Completed Service tests task...

--- UnitTesting - Exercise/Service.Tests/DeviceTests.cs
+++ UnitTesting - Exercise/Service.Tests/DeviceTests.cs
@@ -1,18 +1,325 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using NUnit.Framework;
+using Service.Models.Devices;
+using Service.Models.Parts;
 
 namespace Service.Tests
 {
     public class DeviceTests
     {
+        private Laptop laptop;
+        private PC pc;
+        private Phone phone;
+
         [SetUp]
         public void Setup()
         {
         }
 
         [Test]
-        public void Test1()
+        public void LaptopConstructorShouldSetMakeToTheGivenMake()
         {
-            Assert.Pass();
+            this.laptop = new Laptop("Asus");
+            string expectedMake = "Asus";
+
+            string actualMake = this.laptop.Make;
+
+            Assert.AreEqual(expectedMake, actualMake);
+        }
+
+        [Test]
+        public void LaptopConstructorShouldInitializePartsCollection()
+        {
+            this.laptop = new Laptop("Asus");
+
+            Assert.IsNotNull(this.laptop.Parts);
+        }
+
+        [Test]
+        public void PcConstructorShouldSetMakeToTheGivenMake()
+        {
+            this.pc = new PC("Asus");
+            string expectedMake = "Asus";
+
+            string actualMake = this.pc.Make;
+
+            Assert.AreEqual(expectedMake, actualMake);
+        }
+
+        [Test]
+        public void PcConstructorShouldInitializePartsCollection()
+        {
+            this.pc = new PC("Asus");
+
+            Assert.IsNotNull(this.pc.Parts);
+        }
+
+        [Test]
+        public void PhoneConstructorShouldSetMakeToTheGivenMake()
+        {
+            this.phone = new Phone("Nexus");
+            string expectedMake = "Nexus";
+
+            string actualMake = this.phone.Make;
+
+            Assert.AreEqual(expectedMake, actualMake);
+        }
+
+        [Test]
+        public void PhoneConstructorShouldInitializePartsCollection()
+        {
+            this.phone = new Phone("Nexus");
+
+            Assert.IsNotNull(this.phone.Parts);
+        }
+
+        [Test]
+        public void LaptopMakeShouldThrowArgumentExceptionWhenSettingNullOrEmpty()
+        {
+            string make = null;
+
+            Assert
+                .That(
+                () => this.laptop = new Laptop(make),
+                Throws
+                .ArgumentException
+                .With
+                .Message
+                .EqualTo("Make cannot be null or empty!"));
+        }
+
+        [Test]
+        public void PcMakeShouldThrowArgumentExceptionWhenSettingNullOrEmpty()
+        {
+            string make = null;
+
+            Assert
+                .That(
+                () => this.pc = new PC(make),
+                Throws
+                .ArgumentException
+                .With
+                .Message
+                .EqualTo("Make cannot be null or empty!"));
+        }
+
+        [Test]
+        public void PhoneMakeShouldThrowArgumentExceptionWhenSettingNullOrEmpty()
+        {
+            string make = null;
+
+            Assert
+                .That(
+                () => this.phone = new Phone(make),
+                Throws
+                .ArgumentException
+                .With
+                .Message
+                .EqualTo("Make cannot be null or empty!"));
+        }
+
+        [Test]
+        public void LaptopAddPartShouldThrowInvalidOperationExceptionWhenAddingPartThatIsNotLaptopPart()
+        {
+            this.laptop = new Laptop("Asus");
+            var partToAdd = new PCPart("RAM", 100m);
+
+            Assert
+                .That(
+                () => this.laptop.AddPart(partToAdd),
+                Throws
+                .InvalidOperationException
+                .With
+                .Message
+                .Contains("You cannot add "));
+        }
+
+        [Test]
+        public void PcAddPartShouldThrowInvalidOperationExceptionWhenAddingPartThatIsNotPcPart()
+        {
+            this.pc = new PC("Asus");
+            var partToAdd = new LaptopPart("RAM", 100m);
+
+            Assert
+                .That(
+                () => this.pc.AddPart(partToAdd),
+                Throws
+                .InvalidOperationException
+                .With
+                .Message
+                .Contains("You cannot add "));
+        }
+
+        [Test]
+        public void PhoneAddPartShouldThrowInvalidOperationExceptionWhenAddingPartThatIsNotPhonePart()
+        {
+            this.phone = new Phone("Nexus");
+            var partToAdd = new PCPart("RAM", 100m);
+
+            Assert
+                .That(
+                () => this.phone.AddPart(partToAdd),
+                Throws
+                .InvalidOperationException
+                .With
+                .Message
+                .Contains("You cannot add "));
+        }
+
+        [Test]
+        public void LaptopAddPartShouldThrowInvalidOperationExceptionWhenSamePartAlreadyExists()
+        {
+            this.laptop = new Laptop("Asus");
+            var part = new LaptopPart("RAM", 100m);
+            this.laptop.AddPart(part);
+
+            Assert
+                .That(
+                () => this.laptop.AddPart(part),
+                Throws
+                .InvalidOperationException
+                .With
+                .Message
+                .EqualTo("This part already exists!"));
+        }
+
+        [Test]
+        public void PcAddPartShouldThrowInvalidOperationExceptionWhenSamePartAlreadyExists()
+        {
+            this.pc = new PC("Asus");
+            var part = new PCPart("RAM", 100m);
+            this.pc.AddPart(part);
+
+            Assert
+                .That(
+                () => this.pc.AddPart(part),
+                Throws
+                .InvalidOperationException
+                .With
+                .Message
+                .EqualTo("This part already exists!"));
+        }
+
+        [Test]
+        public void PhoneAddPartShouldThrowInvalidOperationExceptionWhenSamePartAlreadyExists()
+        {
+            this.phone = new Phone("Asus");
+            var part = new PhonePart("Wi-fi", 100m);
+            this.phone.AddPart(part);
+
+            Assert
+                .That(
+                () => this.phone.AddPart(part),
+                Throws
+                .InvalidOperationException
+                .With
+                .Message
+                .EqualTo("This part already exists!"));
+        }
+
+        [Test]
+        public void DeviceRemovePartShouldThrowArgumentExceptionWhenPartToRemoveNameIsNullOrEmpty()
+        {
+            string partToRemoveName = null;
+            var device = new Laptop("Asus");
+
+            Assert
+                .That(
+                () => device.RemovePart(partToRemoveName),
+                Throws
+                .ArgumentException
+                .With
+                .Message
+                .EqualTo("Part name cannot be null or empty!"));
+        }
+
+        [Test]
+        public void DeviceRemovePartShouldThrowInvalidOperationExceptionWhenPartToRemoveIsNotFound()
+        {
+            var device = new Laptop("Asus");
+            var nonExsistingPart = new LaptopPart("RAM", 100m);
+
+            Assert
+                .That(
+                () => device.RemovePart(nonExsistingPart.Name),
+                Throws
+                .InvalidOperationException
+                .With
+                .Message
+                .EqualTo("You cannot remove part which does not exist!"));
+        }
+
+        [Test]
+        public void DeviceRemovePartShouldRemoveThePart()
+        {
+            var device = new Laptop("Asus");
+            var part = new LaptopPart("RAM", 100m);
+            device.AddPart(part);
+
+            device.RemovePart(part.Name);
+
+            Assert.IsTrue(device.Parts.FirstOrDefault(p => p.Name == part.Name) == null);
+        }
+
+        [Test]
+        public void DeviceRepairPartShouldThrowArgumentExceptionWhenPartToRepairNameIsNullOrEmpty()
+        {
+            string partToRepairName = null;
+            var device = new Laptop("Asus");
+
+            Assert
+                .That(
+                () => device.RepairPart(partToRepairName),
+                Throws
+                .ArgumentException
+                .With
+                .Message
+                .EqualTo("Part name cannot be null or empty!"));
+        }
+
+        [Test]
+        public void DeviceRepairPartShouldThrowInvalidOperationExceptionWhenPartToRepairIsNotFound()
+        {
+            var device = new Laptop("Asus");
+            var nonExsistingPart = new LaptopPart("RAM", 100m);
+
+            Assert
+                .That(
+                () => device.RepairPart(nonExsistingPart.Name),
+                Throws
+                .InvalidOperationException
+                .With
+                .Message
+                .EqualTo("You cannot repair part which does not exist!"));
+        }
+
+        [Test]
+        public void DeviceRepairPartShouldThrowInvalidOperationExceptionWhenPartToRepairIsNotBroken()
+        {
+            var part = new LaptopPart("RAM", 100m);
+            var device = new Laptop("Asus");
+            device.AddPart(part);
+
+            Assert
+                .That(
+                () => device.RepairPart(part.Name),
+                Throws
+                .InvalidOperationException
+                .With
+                .Message
+                .EqualTo("You cannot repair part which is not broken!"));
+        }
+
+        [Test]
+        public void DeviceRepairPartShoulTurnIsBrokenToFalse()
+        {
+            var part = new LaptopPart("RAM", 10m, true);
+            var device = new Laptop("Asus");
+            device.AddPart(part);
+
+            device.RepairPart(part.Name);
+
+            Assert.IsFalse(device.Parts.FirstOrDefault(p => p.Name == part.Name).IsBroken);
         }
     }
 }
--- UnitTesting - Exercise/Service.Tests/PartTests.cs
+++ UnitTesting - Exercise/Service.Tests/PartTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using Service.Models.Parts;
 
 namespace Tests
 {
@@ -10,9 +11,333 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        public void LaptopPartConstructorWithTwoParametersShouldSetNameCorrectly()
         {
-            Assert.Pass();
+            var laptopPart = new LaptopPart("Ram", 100m);
+            string expectedName = "Ram";
+
+            string actualName = laptopPart.Name;
+
+            Assert.AreEqual(expectedName, actualName);
+        }
+
+        [Test]
+        public void LaptopPartConstructorWithThreeParametersShouldSetNameCorrectly()
+        {
+            var laptopPart = new LaptopPart("Ram", 100m, true);
+            string expectedName = "Ram";
+
+            string actualName = laptopPart.Name;
+
+            Assert.AreEqual(expectedName, actualName);
+        }
+
+        [Test]
+        public void PCPartConstructorWithTwoParametersShouldSetNameCorrectly()
+        {
+            var PCPart = new PCPart("Ram", 100m);
+            string expectedName = "Ram";
+
+            string actualName = PCPart.Name;
+
+            Assert.AreEqual(expectedName, actualName);
+        }
+
+        [Test]
+        public void PCPartConstructorWithThreeParametersShouldSetNameCorrectly()
+        {
+            var PCPart = new PCPart("Ram", 100m, true);
+            string expectedName = "Ram";
+
+            string actualName = PCPart.Name;
+
+            Assert.AreEqual(expectedName, actualName);
+        }
+
+        [Test]
+        public void PhonePartConstructorWithTwoParametersShouldSetNameCorrectly()
+        {
+            var PhonePart = new PhonePart("Ram", 100m);
+            string expectedName = "Ram";
+
+            string actualName = PhonePart.Name;
+
+            Assert.AreEqual(expectedName, actualName);
+        }
+
+        [Test]
+        public void PhonePartConstructorWithThreeParametersShouldSetNameCorrectly()
+        {
+            var PhonePart = new PhonePart("Ram", 100m, true);
+            string expectedName = "Ram";
+
+            string actualName = PhonePart.Name;
+
+            Assert.AreEqual(expectedName, actualName);
+        }
+
+        [Test]
+        public void LaptopPartConstructorWithTwoParametersShouldSetCostCorrectly()
+        {
+            var laptopPart = new LaptopPart("ram", 100m);
+            decimal expectedCost = 100m * 1.5m;
+
+            decimal actualCost = laptopPart.Cost;
+
+            Assert.AreEqual(expectedCost, actualCost);
+        }
+
+        [Test]
+        public void LaptopPartConstructorWithThreeParametersShouldSetCostCorrectly()
+        {
+            var laptopPart = new LaptopPart("ram", 100m, true);
+            decimal expectedCost = 100m * 1.5m;
+
+            decimal actualCost = laptopPart.Cost;
+
+            Assert.AreEqual(expectedCost, actualCost);
+        }
+
+        [Test]
+        public void PCPartConstructorWithTwoParametersShouldSetCostCorrectly()
+        {
+            var PCPart = new PCPart("ram", 100m);
+            decimal expectedCost = 100m * 1.2m;
+
+            decimal actualCost = PCPart.Cost;
+
+            Assert.AreEqual(expectedCost, actualCost);
+        }
+
+        [Test]
+        public void PCPartConstructorWithThreeParametersShouldSetCostCorrectly()
+        {
+            var PCPart = new PCPart("ram", 100m, true);
+            decimal expectedCost = 100m * 1.2m;
+
+            decimal actualCost = PCPart.Cost;
+
+            Assert.AreEqual(expectedCost, actualCost);
+        }
+
+        [Test]
+        public void PhonePartConstructorWithTwoParametersShouldSetCostCorrectly()
+        {
+            var PhonePart = new PhonePart("ram", 100m);
+            decimal expectedCost = 100m * 1.3m;
+
+            decimal actualCost = PhonePart.Cost;
+
+            Assert.AreEqual(expectedCost, actualCost);
+        }
+
+        [Test]
+        public void PhonePartConstructorWithThreeParametersShouldSetCostCorrectly()
+        {
+            var PhonePart = new PhonePart("ram", 100m, true);
+            decimal expectedCost = 100m * 1.3m;
+
+            decimal actualCost = PhonePart.Cost;
+
+            Assert.AreEqual(expectedCost, actualCost);
+        }
+
+        [Test]
+        public void LaptopPartConstructorWithTwoParametersShouldSetIsBrokenCorrectly()
+        {
+            var laptopPart = new LaptopPart("ram", 100m);
+
+            bool actualIsBroken = laptopPart.IsBroken;
+
+            Assert.IsFalse(actualIsBroken);
+        }
+
+        [Test]
+        public void LaptopPartConstructorWithThreeParametersShouldSetIsBrokenCorrectly()
+        {
+            var laptopPart = new LaptopPart("ram", 100m, true);
+
+            bool actualIsBroken = laptopPart.IsBroken;
+
+            Assert.IsTrue(actualIsBroken);
+        }
+
+        [Test]
+        public void PCPartConstructorWithTwoParametersShouldSetIsBrokenCorrectly()
+        {
+            var PCPart = new PCPart("ram", 100m);
+
+            bool actualIsBroken = PCPart.IsBroken;
+
+            Assert.IsFalse(actualIsBroken);
+        }
+
+        [Test]
+        public void PCPartConstructorWithThreeParametersShouldSetIsBrokenCorrectly()
+        {
+            var PCPart = new PCPart("ram", 100m, true);
+
+            bool actualIsBroken = PCPart.IsBroken;
+
+            Assert.IsTrue(actualIsBroken);
+        }
+
+        [Test]
+        public void PhonePartConstructorWithTwoParametersShouldSetIsBrokenCorrectly()
+        {
+            var PhonePart = new PhonePart("ram", 100m);
+
+            bool actualIsBroken = PhonePart.IsBroken;
+
+            Assert.IsFalse(actualIsBroken);
+        }
+
+        [Test]
+        public void PhonePartConstructorWithThreeParametersShouldSetIsBrokenCorrectly()
+        {
+            var PhonePart = new PhonePart("ram", 100m, true);
+
+            bool actualIsBroken = PhonePart.IsBroken;
+
+            Assert.IsTrue(actualIsBroken);
+        }
+
+        [Test]
+        public void LaptopPartNameShouldThrowArgumentExceptionWhenSettingWithNullOrEmpty()
+        {
+            Assert
+                .That(
+                () => new LaptopPart(null, 100m),
+                Throws
+                .ArgumentException
+                .With
+                .Message
+                .EqualTo("Part name cannot be null or empty!"));
+        }
+
+        [Test]
+        public void PCPartNameShouldThrowArgumentExceptionWhenSettingWithNullOrEmpty()
+        {
+            Assert
+                .That(
+                () => new PCPart(null, 100m),
+                Throws
+                .ArgumentException
+                .With
+                .Message
+                .EqualTo("Part name cannot be null or empty!"));
+        }
+
+        [Test]
+        public void PhonePartNameShouldThrowArgumentExceptionWhenSettingWithNullOrEmpty()
+        {
+            Assert
+                .That(
+                () => new PhonePart(null, 100m),
+                Throws
+                .ArgumentException
+                .With
+                .Message
+                .EqualTo("Part name cannot be null or empty!"));
+        }
+
+        [Test]
+        public void LaptopPartCostShouldThrowArgumentExceptionWhenSettingWithZeroOrNegative()
+        {
+            Assert
+                .That(
+                () => new LaptopPart("ram", -5),
+                Throws
+                .ArgumentException
+                .With
+                .Message
+                .EqualTo("Part cost cannot be zero or negative!"));
+        }
+
+        [Test]
+        public void PCPartCostShouldThrowArgumentExceptionWhenSettingWithZeroOrNegative()
+        {
+            Assert
+                .That(
+                () => new PCPart("ram", -5),
+                Throws
+                .ArgumentException
+                .With
+                .Message
+                .EqualTo("Part cost cannot be zero or negative!"));
+        }
+
+        [Test]
+        public void PhonePartCostShouldThrowArgumentExceptionWhenSettingWithZeroOrNegative()
+        {
+            Assert
+                .That(
+                () => new PhonePart("ram", -5),
+                Throws
+                .ArgumentException
+                .With
+                .Message
+                .EqualTo("Part cost cannot be zero or negative!"));
+        }
+
+        [Test]
+        public void LaptopPartRepairShouldTurnIsBrokenToFalse()
+        {
+            var laptopPart = new LaptopPart("ram", 100m, true);
+
+            laptopPart.Repair();
+
+            Assert.IsFalse(laptopPart.IsBroken);
+        }
+
+        [Test]
+        public void PCPartRepairShouldTurnIsBrokenToFalse()
+        {
+            var PCPart = new PCPart("ram", 100m, true);
+
+            PCPart.Repair();
+
+            Assert.IsFalse(PCPart.IsBroken);
+        }
+
+        [Test]
+        public void PhonePartRepairShouldTurnIsBrokenToFalse()
+        {
+            var phonePart = new PhonePart("ram", 100m, true);
+
+            phonePart.Repair();
+
+            Assert.IsFalse(phonePart.IsBroken);
+        }
+
+        [Test]
+        public void LaptopPartReportShouldReturnStringContainingNameCostAndIsBrokenPropertiesAsPartOfIt()
+        {
+            var laptopPart = new LaptopPart("ram", 100m, true);
+
+            StringAssert.Contains($"{laptopPart.Name}", laptopPart.Report(), "Report string doesn't contain Name property");
+            StringAssert.Contains($"{laptopPart.Cost}", laptopPart.Report(), "Report string doesn't contain Cost property");
+            StringAssert.Contains($"{laptopPart.IsBroken}", laptopPart.Report(), "Report string doesn't contain IsBroken property");
+        }
+
+        [Test]
+        public void PCPartReportShouldReturnStringContainingNameCostAndIsBrokenPropertiesAsPartOfIt()
+        {
+            var pcPart = new PCPart("ram", 100m, true);
+
+            StringAssert.Contains($"{pcPart.Name}", pcPart.Report(), "Report string doesn't contain Name property");
+            StringAssert.Contains($"{pcPart.Cost}", pcPart.Report(), "Report string doesn't contain Cost property");
+            StringAssert.Contains($"{pcPart.IsBroken}", pcPart.Report(), "Report string doesn't contain IsBroken property");
+        }
+
+        [Test]
+        public void PhonePartReportShouldReturnStringContainingNameCostAndIsBrokenPropertiesAsPartOfIt()
+        {
+            var phonePart = new PhonePart("ram", 100m, true);
+
+            StringAssert.Contains($"{phonePart.Name}", phonePart.Report(), "Report string doesn't contain Name property");
+            StringAssert.Contains($"{phonePart.Cost}", phonePart.Report(), "Report string doesn't contain Cost property");
+            StringAssert.Contains($"{phonePart.IsBroken}", phonePart.Report(), "Report string doesn't contain IsBroken property");
         }
     }
 }
